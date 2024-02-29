27d70cea5213c9683478aee964d26679f6fd3742
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2024-02-28 19:35:25 -0500

    Proyecto de programación avanzada con gradle

---
+++ src/main/java/com/test/avanzada/AvanzadaApplication.java
@@ -0,0 +1,13 @@
+package com.test.avanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanzadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanzadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/avanzada/AvanzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.avanzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanzadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
